f8dd56892cbb774bff0f63aa0a0f52dc4330c06f
Ole Stavland <olestavland@Stavland-2.uia.no>
2024-01-15 12:55:36 +0100

    Added the joke with colors

---
+++ app.mjs
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+
+const log = console.log;
+
+log(chalk.blue("Hva"), chalk.green("er"), 
+chalk.red("beste"), chalk.yellow("vin"), 
+chalk.magenta("fra"),
+ chalk.cyanBright("sydpolen?"),
+chalk.bgRed(" Pingvin!")
+);
+
+
